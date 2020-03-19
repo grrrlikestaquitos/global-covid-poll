12a8b6a2a5b10154719fd8bf8be66046cde47d7e
Andrei <andreicrimson@gmail.com>
2020-03-19 02:59:47 -0700

    created build

---
+++ client/build/precache-manifest.e2f2845d55ae63d12718e8993d88f101.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90427c8c224c0ba27c2a7cb10c30a185",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d485c73fa4d9a273e27",
+    "url": "/static/css/main.0e2a370a.chunk.css"
+  },
+  {
+    "revision": "90ec06cde7a3103aa849",
+    "url": "/static/js/2.27ab9a78.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.27ab9a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d485c73fa4d9a273e27",
+    "url": "/static/js/main.8eb738be.chunk.js"
+  },
+  {
+    "revision": "7a33af610fb006a7402f",
+    "url": "/static/js/runtime-main.990cff24.js"
+  },
+  {
+    "revision": "56822d598b83a9051a0d8666d89f8c38",
+    "url": "/static/media/covid_icon.56822d59.png"
+  }
+]);
